0b3b0a31c5f0a43eb8fda06d2da276b1f7877fc5
Mcnoble <idowufestustemiloluwa@gmail..com>
2023-03-21 00:59:54 +0100

    feat: updated partner section :octocat:

--- Web/components/PartnerSection.jsx
+++ Web/components/PartnerSection.jsx
@@ -6,11 +6,11 @@ import filecoin from "../images/filecoin.png"
 import chainlink from "../images/chainlink.jpg"
 function PartnerSection() {
   return (
-    <div className='flex items-center justify-around lg:flex-nowrap flex-wrap px-10'>
-        <div><a href='https://polygon.technology/' className="link link-hover" target={'_blank'} rel="noreferrer"><Image src={polygon}/></a></div>
-        <div><a href='https://filecoin.io/' className="link link-hover" target={'_blank'} rel="noreferrer"><Image src={filecoin}/></a></div>
-        <div><a href='https://chain.link/' className="link link-hover" target={'_blank'} rel="noreferrer"><Image src={chainlink}/></a></div>
-        <div><a href='https://www.microsoft.com/en-us/startups' className="link link-hover" target={'_blank'} rel="noreferrer"><Image src={microsoft}/></a></div>
+    <div className='flex items-center justify-evenly flex-wrap lg:flex-nowrap px-5 mb-10'>
+        <div className='w-1/2 lg:w-1/4'><a href='https://polygon.technology/' className="link link-hover" target={'_blank'} rel="noreferrer"><Image src={polygon}/></a></div>
+        <div className='w-1/2 lg:w-1/4'><a href='https://filecoin.io/' className="link link-hover" target={'_blank'} rel="noreferrer"><Image src={filecoin}/></a></div>
+        <div className='w-1/2 lg:w-1/4'><a href='https://chain.link/' className="link link-hover" target={'_blank'} rel="noreferrer"><Image src={chainlink}/></a></div>
+        <div className='w-1/2 lg:w-1/4'><a href='https://www.microsoft.com/en-us/startups' className="link link-hover" target={'_blank'} rel="noreferrer"><Image src={microsoft}/></a></div>
     </div>
     
   )
